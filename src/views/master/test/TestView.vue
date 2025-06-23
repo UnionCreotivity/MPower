27b19355574c3a1775c4bff94fa0bf260a98ad35
jshan <jshan0309@hotmail.com>
2025-06-23 11:25:01 +0800

    fix gitignore

--- src/views/master/test/TestView.vue
+++ src/views/master/test/TestView.vue
@@ -1,20 +1,12 @@
 <template>
   <section class="test-view">
-    <video
-      autoplay
-      loop
-      muted
-      preload="auto"
-      width="640"
-      height="360"
-      src="../../../assets/video/test.mp4"
-    ></video>
+    <video autoplay loop muted preload="auto" width="640" height="360" :src="videoSrc"></video>
   </section>
 </template>
 
 <script setup lang="ts">
 import '@/assets/scss/metro/_arial-photo.scss'
-// import videoSrc from '@/assets/video/test.mp4'
+import videoSrc from '@/assets/video/test.mp4'
 </script>
 
 <style scoped></style>
