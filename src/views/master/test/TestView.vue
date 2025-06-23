63b7122d27e7d6df8dcc3654956e751badf3db2d
jshan <jshan0309@hotmail.com>
2025-06-23 11:16:39 +0800

    改路徑

--- src/views/master/test/TestView.vue
+++ src/views/master/test/TestView.vue
@@ -1,12 +1,20 @@
 <template>
   <section class="test-view">
-    <video autoplay loop muted preload="auto" width="640" height="360" :src="videoSrc"></video>
+    <video
+      autoplay
+      loop
+      muted
+      preload="auto"
+      width="640"
+      height="360"
+      src="../../../assets/video/test.mp4"
+    ></video>
   </section>
 </template>
 
 <script setup lang="ts">
 import '@/assets/scss/metro/_arial-photo.scss'
-import videoSrc from '@/assets/video/test.mp4'
+// import videoSrc from '@/assets/video/test.mp4'
 </script>
 
 <style scoped></style>
