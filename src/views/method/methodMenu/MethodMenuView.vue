2524bf78c670d44bcbf03e337f63448dbb7cd910
jshan <jshan0309@hotmail.com>
2025-06-27 11:20:55 +0800

    feat. 圖片hover 波浪效果

--- src/views/method/methodMenu/MethodMenuView.vue
+++ src/views/method/methodMenu/MethodMenuView.vue
@@ -5,31 +5,12 @@
       <div class="en">METHOD OF HOUSE</div>
     </div>
     <div class="menu-box">
-      <div class="item">
+      <div class="item" v-for="(item, index) in items" :key="index">
         <div class="img-box">
-          <img src="../../../assets/img/method/1.webp" alt="" srcset="" />
-          <div class="text">STRUCTURE</div>
+          <WaveImage :imagePath="item.img" />
+          <div class="text">{{ item.text }}</div>
         </div>
-
-        <div class="title">結構工法</div>
-      </div>
-
-      <div class="item">
-        <div class="img-box">
-          <img src="../../../assets/img/method/2.webp" alt="" srcset="" />
-          <div class="text">WATERPROOF</div>
-        </div>
-
-        <div class="title">防水工法</div>
-      </div>
-
-      <div class="item">
-        <div class="img-box">
-          <img src="../../../assets/img/method/3.webp" alt="" srcset="" />
-          <div class="text">INTIMATE</div>
-        </div>
-
-        <div class="title">貼心工法</div>
+        <div class="title">{{ item.title }}</div>
       </div>
     </div>
   </section>
@@ -37,8 +18,17 @@
 
 <script setup lang="ts">
 import '@/assets/scss/method/_method-menu.scss'
+import WaveImage from '@/components/WaveImage/WaveImage.vue'
 // import { onMounted } from 'vue'
 // import gsap from 'gsap'
+import img1 from '@/assets/img/method/1.webp'
+import img2 from '@/assets/img/method/2.webp'
+import img3 from '@/assets/img/method/3.webp'
+const items = [
+  { img: img1, text: 'STRUCTURE', title: '結構工法' },
+  { img: img2, text: 'WATERPROOF', title: '防水工法' },
+  { img: img3, text: 'INTIMATE', title: '貼心工法' },
+]
 </script>
 
 <style scoped></style>
