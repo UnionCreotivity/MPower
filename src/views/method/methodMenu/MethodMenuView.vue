7193cc259e87477655360c21970618ea9f15a494
jshan <jshan0309@hotmail.com>
2025-06-27 10:59:19 +0800

    feat. 建築工藝目錄完成切版跟hover

--- src/views/method/methodMenu/MethodMenuView.vue
+++ src/views/method/methodMenu/MethodMenuView.vue
@@ -4,6 +4,34 @@
       <div class="zh">建築工藝</div>
       <div class="en">METHOD OF HOUSE</div>
     </div>
+    <div class="menu-box">
+      <div class="item">
+        <div class="img-box">
+          <img src="../../../assets/img/method/1.webp" alt="" srcset="" />
+          <div class="text">STRUCTURE</div>
+        </div>
+
+        <div class="title">結構工法</div>
+      </div>
+
+      <div class="item">
+        <div class="img-box">
+          <img src="../../../assets/img/method/2.webp" alt="" srcset="" />
+          <div class="text">WATERPROOF</div>
+        </div>
+
+        <div class="title">防水工法</div>
+      </div>
+
+      <div class="item">
+        <div class="img-box">
+          <img src="../../../assets/img/method/3.webp" alt="" srcset="" />
+          <div class="text">INTIMATE</div>
+        </div>
+
+        <div class="title">貼心工法</div>
+      </div>
+    </div>
   </section>
 </template>
 
