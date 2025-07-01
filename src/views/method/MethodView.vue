1df317c8105130488c13147fe675583893f33d0a
jshan <jshan0309@hotmail.com>
2025-07-01 15:28:56 +0800

    feat. add 共用content、title

--- src/views/method/MethodView.vue
+++ src/views/method/MethodView.vue
@@ -9,11 +9,17 @@ import { RouterView } from 'vue-router'
 import ViewFixed from '@/components/view-fixed/ViewFixed.vue'
 </script>
 
-<style>
+<style lang="scss">
 #method-view {
   background-color: #f4f3ec;
   width: 100%;
   height: 100svh;
   overflow: hidden;
+  @media all and (max-width: 1400px) {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 }
 </style>
