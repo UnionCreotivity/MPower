7041a36e8be904bfefe8f350c743f9e59626ab32
jshan <jshan0309@hotmail.com>
2025-07-22 13:19:31 +0800

    feat. 換圖

--- src/components/metro/MrtGroup.vue
+++ src/components/metro/MrtGroup.vue
@@ -4,7 +4,7 @@
       <img src="/src/assets/img/metro/red_line.webp" alt="red_line" />
     </div>
     <div class="green-line mrt-line" @click="$emit('open-green-line')">
-      <img src="/src/assets/img/metro/green_line.webp" alt="green_line" />
+      <img src="/src/assets/img/metro/green_line.svg" alt="green_line" />
     </div>
     <div class="blue-line mrt-line">
       <img src="/src/assets/img/metro/blue_line.webp" alt="blue_line" />
