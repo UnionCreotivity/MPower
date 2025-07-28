1c633ee8a6fe4693742d4fc26e7cdff728cbb783
jshan <jshan0309@hotmail.com>
2025-07-28 10:13:17 +0800

    feat. 平面圖改版

--- src/components/Floor/floorMenu/FloorMenu.vue
+++ src/components/Floor/floorMenu/FloorMenu.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="floor-menu">
     <div class="floor-box">
+      <div class="floor-title">FLOOR</div>
       <div
         v-for="floor in floorList"
         :key="floor"
         class="floor"
-        :class="{ hovered: isHovered(floor) }"
-        @mouseenter="hoverFloor = floor"
-        @mouseleave="hoverFloor = null"
         @click="$emit('selectFloor', floor)"
       >
         {{ floor }}
@@ -20,45 +18,7 @@
 import '@/assets/scss/building/_building-view.scss'
 import { ref } from 'vue'
 
-const hoverFloor = ref<string | null>(null)
-
-const floorList = [
-  'R1',
-  '12F',
-  '11F',
-  '10F',
-  '9F',
-  '8F',
-  '7F',
-  '6F',
-  '5F',
-  '4F',
-  '3F',
-  '2F',
-  '1F',
-  'B1',
-  'B2',
-  'B3',
-  'B4',
-]
-
-// 判斷目前這個樓層是否該亮起底色
-function isHovered(floor: string): boolean {
-  const hover = hoverFloor.value
-
-  const isMidFloor = (f: string) => {
-    const num = parseInt(f)
-    return !isNaN(num) && num >= 3 && num <= 12
-  }
-
-  // 如果滑到的是 3F~12F 中任何一層
-  if (hover && isMidFloor(hover)) {
-    return isMidFloor(floor)
-  }
-
-  // 其他情況：只有單一樓層亮
-  return hover === floor
-}
+const floorList = ['R1', '3F-12F', '2F', '1F', 'B1', 'B2', 'B3', 'B4']
 </script>
 
 <style lang="scss"></style>
--- src/components/Floor/floorView/FloorView.vue
+++ src/components/Floor/floorView/FloorView.vue
@@ -13,7 +13,21 @@
     <img src="../../../assets/img/building/night.png" alt="night" />
   </div>
 
+  <div class="bg-text">FLOOR PLAN</div>
   <div class="hint">3D外觀示意圖</div>
+
+  <div class="content-box">
+    <div class="title">俐落線條．理性構築</div>
+    <div class="small-title">以實用為本，少即是多，藏即是顯</div>
+    <div class="content">
+      理性人本為筆，書寫出生活的實用秩序。<br />
+      每一筆俐落，回應城市節奏的韻律。<br />
+      留白沉靜，留下城市呼吸的間隙。<br />
+      低戶數、高隱私；齊質44戶，內斂布局。<br />
+      當空間開始理解人，生活從容生根。
+    </div>
+    <img class="icon" src="../../../assets/img/building/icon.svg" alt="icon" srcset="" />
+  </div>
 </template>
 
 <script setup lang="ts">
