4d2371c3fc292ab10cf2202d7b40e5ed31410a4a
jshan <jshan0309@hotmail.com>
2025-08-07 14:09:03 +0800

    feat. 新增台北捷運彈窗

--- src/components/metro/OrangeLine.vue
+++ src/components/metro/OrangeLine.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="line-popup orange-line-box">
-    <div class="overlay-close" @click="$emit('close')"></div>
+    <div class="overlay-close" @click="closeWithAnimation"></div>
     <div class="main-box">
       <div class="img-box">
         <img src="../../assets/img/metro/orange_line_bg.webp" alt="orange_line_bg" srcset="" />
@@ -30,7 +30,7 @@
           <div class="content">2024年交通部已原則同意，待最終審議後核定</div>
         </div>
 
-        <div class="btn">
+        <div class="btn" @click="showTaipeiLine = true">
           <img src="../../assets/img/metro/taipei_btn.png" alt="" />
         </div>
       </div>
@@ -40,12 +40,103 @@
       </div>
     </div>
   </div>
+
+  <TaipeiLine v-if="showTaipeiLine" @close="showTaipeiLine = false" />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import gsap from 'gsap'
+import TaipeiLine from '@/components/metro/TaipeiLine.vue'
 import '@/assets/scss/metro/_metro-light-box.scss'
+
+// 定義 emit
+const emit = defineEmits(['close'])
+const showTaipeiLine = ref(false)
+
+const imgAni = () => {
+  const tl = gsap.timeline({})
+  tl.fromTo(
+    '.line-popup',
+    {
+      maskPosition: '200% 0',
+    },
+    {
+      maskPosition: '0% 0%',
+      duration: 1.5,
+      ease: 'cubic-bezier(0.64, 0.03, 0.07, 0.97)',
+    },
+  )
+
+    .from(
+      '.line-popup .main-box .img-box img',
+      {
+        duration: 1,
+        opacity: 0,
+        scale: 1.4,
+        ease: 'power0.in',
+      },
+      '<0.2',
+    )
+
+    .from(
+      '.line-popup .main-box .title-box div',
+      {
+        duration: 1,
+        opacity: 0,
+        x: '-110',
+        stagger: 0.13,
+      },
+      '<0.45',
+    )
+    .from(
+      '.line-popup .main-box .main-content-box .list div',
+      {
+        duration: 1,
+        opacity: 0,
+        x: '-90',
+        stagger: 0.1,
+      },
+      '<0.2',
+    )
+    .from(
+      '.line-popup .main-box .main-content-box .btn',
+      {
+        duration: 1,
+        opacity: 0,
+        y: '40',
+      },
+      '<0.9',
+    )
+    .from(
+      '.line-popup .main-box .line-img img',
+      {
+        duration: 1,
+        opacity: 0,
+        x: '-90',
+      },
+      '<-0.35',
+    )
+}
+
+const closeWithAnimation = () => {
+  const tl = gsap.timeline({
+    onComplete: () => {
+      emit('close') // 動畫結束後再真正關閉
+    },
+  })
+
+  tl.to('.line-popup', {
+    maskPosition: '200% 0',
+    duration: 1.2,
+    ease: 'cubic-bezier(0.64, 0.03, 0.07, 0.97)',
+  })
+}
+
+// 初始化時先呼叫一次
+onMounted(async () => {
+  imgAni()
+})
 </script>
 
 <style scoped></style>
---
+++ src/components/metro/TaipeiLine.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="taipei-line-box">
+    <div class="overlay-close" @click="closeWithAnimation"></div>
+    <div class="main-box">
+      <div class="img-box">
+        <img src="../../assets/img/metro/taipei_bg.webp" alt="taipei_bg" srcset="" />
+      </div>
+
+      <div class="title">
+        <img src="../../assets/img/metro/taipei_title.png" alt="taipei_title" srcset="" />
+      </div>
+
+      <div class="taipei-img">
+        <img src="../../assets/img/metro/taipei_img.webp" alt="taipei_img" srcset="" />
+        <div class="hint">
+          資料來源:賣價登錄資料,臺北大眾捷連股份有限公司;永慶房產集團研究發展中心氣整整。<br />
+          住宅價格篩選條件:1.交易標的為房地(土地+建物)、房地(土地+建物)+車位、建物。2.建物型態:住宅大樓、華廈及公寓。3.排除屋齢兩年內、一樓及親友交易之成交資料。<br />
+          註1.實價登錄資料統計自2022年6月至2023年5月。
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import gsap from 'gsap'
+
+import '@/assets/scss/metro/_metro-light-box.scss'
+
+const emit = defineEmits(['close'])
+
+const imgAni = () => {
+  const tl = gsap.timeline({})
+
+  tl.fromTo(
+    '.taipei-line-box',
+    {
+      maskPosition: '200% 0',
+    },
+    {
+      maskPosition: '0% 0%',
+      duration: 1.5,
+      ease: 'cubic-bezier(0.64, 0.03, 0.07, 0.97)',
+    },
+  )
+}
+
+const closeWithAnimation = () => {
+  const tl = gsap.timeline({
+    onComplete: () => {
+      emit('close') // 動畫結束後再真正關閉
+    },
+  })
+
+  tl.to('.taipei-line-box', {
+    maskPosition: '200% 0',
+    duration: 1.2,
+  })
+}
+// 初始化時先呼叫一次
+onMounted(async () => {
+  imgAni()
+})
+</script>
+
+<style scoped></style>
