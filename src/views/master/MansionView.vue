af871546dc35bdfefc20e772e9db7042e0e46801
jshan <jshan0309@hotmail.com>
2025-08-18 16:08:44 +0800

    feat. 新增營建起家

---
+++ src/views/master/MansionView.vue
@@ -0,0 +1,32 @@
+<template>
+  <ViewFixed id="masion-view">
+    <router-view></router-view>
+  </ViewFixed>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import ViewFixed from '@/components/view-fixed/ViewFixed.vue'
+</script>
+
+<style lang="scss">
+#masion-view {
+  background-color: #f4f3ec;
+  width: 100%;
+  height: 100svh;
+  overflow: hidden;
+  @media all and (max-width: 1400px) {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .full-screen {
+    @media all and (max-width: 1400px) {
+      right: 2vw;
+      bottom: 4%;
+    }
+  }
+}
+</style>
--- src/views/metier/buildOrigin/BuildOriginView.vue
+++ src/views/metier/buildOrigin/BuildOriginView.vue
@@ -2,18 +2,25 @@
   <section class="build-origin">
     <div class="top-box">
       <div class="left">
+        <img
+          class="line"
+          src="../../../assets/img/metier/build-origin/line.png"
+          alt="line"
+          srcset=""
+        />
         <div class="icon">
           <img src="../../../assets/img/metier/build-origin/bear.webp" alt="" srcset="" />
         </div>
         <div class="title-box">
-          <div class="title">真正好住懂住<br />大熊擇學昇華</div>
-          <div class="en-title">
+          <div class="title" ref="title">真正好住懂住<br />大熊擇學昇華</div>
+          <div class="en-title" ref="enTitle">
             BUILT FROM<br />
             CONSTRUCTION
           </div>
         </div>
       </div>
-      <div class="right">
+
+      <div class="right" ref="content">
         精華擇地學，不只便捷更是稀有。<br />
         延續14期繁華，又一精選地段。<br />
         公設美學輕奢為上，精準格局風光為美。<br />
@@ -24,12 +31,86 @@
     <div class="bottom-box">
       <div class="img-box">
         <img src="../../../assets/img/metier/build-origin/1.webp" alt="" srcset="" />
+        <div class="hint">情境示意圖</div>
       </div>
     </div>
     <FullScreen :forceWhiteIcon="true" />
   </section>
 </template>
+
 <script setup lang="ts">
 import '@/assets/scss/metier/_build-origin.scss'
 import FullScreen from '@/components/full-screen/FullScreen.vue'
+import { ref, onMounted } from 'vue'
+import gsap from 'gsap'
+import SplitText from 'gsap/SplitText'
+gsap.registerPlugin(SplitText)
+const title = ref<HTMLElement | null>(null)
+const enTitle = ref<HTMLElement | null>(null)
+const content = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  const tl = gsap.timeline({ delay: 0.5 })
+
+  const splitTitle = new SplitText(title.value, { type: 'chars' })
+  const splitenTitle = new SplitText(enTitle.value, { type: 'chars' })
+  const splitContent = new SplitText(content.value, { type: 'lines' })
+
+  tl.from('.build-origin .top-box .left .icon', {
+    duration: 1,
+    opacity: 0,
+    y: 50,
+  })
+    .from(
+      splitTitle.chars,
+      {
+        duration: 1,
+        y: 50,
+        opacity: 0,
+        stagger: 0.05,
+      },
+      '0.3',
+    )
+    .from(
+      splitenTitle.chars,
+      {
+        duration: 1.2,
+        opacity: 0,
+        y: 80,
+        rotateX: -90,
+        rotateY: 90,
+        stagger: 0.02,
+      },
+      '<0.4',
+    )
+    .from(
+      splitContent.lines,
+      {
+        duration: 1,
+        opacity: 0,
+        y: 80,
+        stagger: 0.15,
+      },
+      '<0.6',
+    )
+    .to(
+      '.build-origin .top-box .left .line',
+      {
+        maskPosition: '0% 0%',
+        duration: 1,
+      },
+      '<0.6',
+    )
+
+    .to(
+      '.build-origin .bottom-box .img-box img',
+      {
+        clipPath: 'inset(0% 0% 0% 0)',
+        scale: 1,
+        duration: 1.3,
+        // opacity: 1,
+      },
+      '<0.5',
+    )
+})
 </script>
