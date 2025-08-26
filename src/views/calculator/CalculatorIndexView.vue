2dbde65027526ff88e7af1a7d4ed8a7be5c47274
jshan <jshan0309@hotmail.com>
2025-08-26 17:08:51 +0800

    feat. 修改列印有目錄問題

--- src/views/calculator/CalculatorIndexView.vue
+++ src/views/calculator/CalculatorIndexView.vue
@@ -11,11 +11,17 @@ import ViewFixed from '@/components/view-fixed/ViewFixed.vue'
 import FullScreen from '@/components/full-screen/FullScreen.vue'
 </script>
 
-<style>
+<style lang="scss">
 #calculator-index-view {
   background-color: #f4f3ec;
   width: 100%;
   height: 100svh;
   overflow: hidden;
+  .menu-icon-container,
+  .full-screen {
+    @media print {
+      display: none;
+    }
+  }
 }
 </style>
